3cb515be6ec0fc01ae7e7466b85c1703567c8ff9
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-02-15 22:50:38 -0400

    Adiciona botao de apagar e tentativas de consertar requisito 4

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function selectPalets() {
   const paletsColor = document.querySelector('#color-palette');
   paletsColor.addEventListener('click', function(event) {
-    let selectedPalet = document.querySelector('.selected');
+    const selectedPalet = document.querySelector('.selected');
     selectedPalet.classList.remove('selected');
     event.target.classList.add('selected');
   })
 }
 selectPalets()
 
-let pixelSelect = document.getElementsByClassName('pixel');
-let classSelect = document.getElementsByClassName('selected');
+const pixelSelect = document.getElementsByClassName('pixel');
+const classSelect = document.getElementsByClassName('selected');
 function changePixelColor(){
   for (let i = 0; i < pixelSelect.length; i += 1){
     pixelSelect[i].addEventListener('click', function(event){
-      let selectedBackgroundColor = window.getComputedStyle(classSelect[0]);
+      const selectedBackgroundColor = window.getComputedStyle(classSelect[0]);
       if (!event.target.className.backgroundColor === 'white'){
         event.target.style.backgroundColor = 'white';
       } else {event.target.style.backgroundColor = selectedBackgroundColor.getPropertyValue('background-color');
@@ -22,3 +22,11 @@ function changePixelColor(){
   }
 }
 changePixelColor();
+
+function limparPixels() {
+  const listPixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < listPixels.length; i += 1) {
+    listPixels[i].style.backgroundColor = 'rgba(255, 255, 255)';
+  }
+}
+document.querySelector('#clear-board').addEventListener('click', limparPixels);
